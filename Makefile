54204d8b40a4a524826bff4c5bf10ec0326042a5
John Mettraux <jmettraux@gmail.com>
2020-12-07 20:12:25 +0900

    Complete compute_grid()

--- Makefile
+++ Makefile
@@ -5,5 +5,5 @@ t1:
 	ruby -Ilib -rarchelaus -e "p Archelaus.compute_distance(50.1, -5.0, 58.3, -3.0)"
 	ruby -Ilib -rarchelaus -e "p Archelaus.compute_bearing(50.1, -5.0, 58.3, -3.0)"
 t2:
-	ruby -Ilib -rarchelaus -e "p Archelaus.compute_grid(52.204, 0.142, 100, 10, 2)"
+	ruby -Ilib -rpp -rarchelaus -e "pp Archelaus.compute_grid(52.204, 0.142, 100, 10, 2)"
 
--- lib/archelaus/hexgrid.rb
+++ lib/archelaus/hexgrid.rb
@@ -3,21 +3,26 @@ module Archelaus
 
   class << self
 
-    def compute_row(lat, lon, step, width)
+    def compute_line(lat, lon, step, bearing, count)
 
       lat1, lon1 = lat, lon
 
       [ [ lat, lon ] ] +
-      (width - 1).times
+      (count - 1).times
         .collect {
-          lat1, lon1 = compute_point(lat1, lon1, 90.0, step)
+          lat1, lon1 = compute_point(lat1, lon1, bearing, step)
           [ lat1, lon1 ] }
     end
 
     def compute_grid(lat, lon, step, width, height)
 
-      compute_row(lat, lon, step, width)
+      compute_line(lat, lon, step, 180.0, height)
+        .collect { |lat0, lon0| compute_line(lat0, lon0, step, 90.0, width) }
     end
   end
 end
+
+
+# 300 by 300
+# 30km by 30km
 
