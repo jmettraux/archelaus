fba06248f61626a4ea2c335d2064dc0cbddc83ad
John Mettraux <jmettraux@gmail.com>
2020-12-07 21:01:19 +0900

    Go from square to hex

--- lib/archelaus/hexgrid.rb
+++ lib/archelaus/hexgrid.rb
@@ -6,17 +6,19 @@ module Archelaus
     def compute_line(lat, lon, step, bearing, count)
 
       lat1, lon1 = lat, lon
+      bearings = Array(bearing)
 
       [ [ lat, lon ] ] +
       (count - 1).times
-        .collect {
-          lat1, lon1 = compute_point(lat1, lon1, bearing, step)
+        .collect { |i|
+          lat1, lon1 =
+            compute_point(lat1, lon1, bearings[i % bearings.length], step)
           [ lat1, lon1 ] }
     end
 
     def compute_grid(lat, lon, step, width, height)
 
-      compute_line(lat, lon, step, 180.0, height)
+      compute_line(lat, lon, step, [ 150.0, 210.0 ], height)
         .collect { |lat0, lon0| compute_line(lat0, lon0, step, 90.0, width) }
     end
   end
