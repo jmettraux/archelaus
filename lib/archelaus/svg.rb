165e89a99025ce916e04eef9cc6923b3d5a6340a
John Mettraux <jmettraux@gmail.com>
2020-12-17 19:57:59 +0900

    Move svg.js to its own file

--- lib/archelaus/svg.rb
+++ lib/archelaus/svg.rb
@@ -231,64 +231,8 @@ use[href="#H"] {
         end
       end
 
-      maken(body, :script) << Ox::Raw.new(%{
-var clog = console.log;
-
-var svg = document.getElementById('svg-map');
-var inc = 1000;
-
-function getViewBox() {
-
-  var vb = svg.getAttribute('viewBox');
-  var m = vb.match(/(-?[0-9]+) (-?[0-9]+) (-?[0-9]+) (-?[0-9]+)/);
-
-  return {
-    x: parseInt(m[1], 10), y: parseInt(m[2], 10),
-    w: parseInt(m[3], 10), h: parseInt(m[4], 10) }
-};
-
-document.body.addEventListener('keyup', function(ev) {
-
-  var vb = getViewBox();
-
-  var c = ev.keyCode;
-       if (c === 72) { vb.x = vb.x - inc; }
-  else if (c === 74) { vb.y = vb.y + inc; }
-  else if (c === 75) { vb.y = vb.y - inc; }
-  else if (c === 76) { vb.x = vb.x + inc; }
-  else if (c === 78 || c === 34) { vb.w = vb.w + inc; vb.h = vb.h + inc; }
-  else if (c === 77 || c === 33) { vb.w = vb.w - inc; vb.h = vb.h - inc; }
-  else if (c === 37) { vb.x = vb.x - (inc / 10); }
-  else if (c === 39) { vb.x = vb.x + (inc / 10); }
-  else if (c === 38) { vb.y = vb.y - (inc / 10); }
-  else if (c === 40) { vb.y = vb.y + (inc / 10); }
-  else { clog(c); return; }
-
-  svg.setAttribute(
-    'viewBox',
-    '' + vb.x + ' ' + vb.y + ' ' + vb.w + ' ' + vb.h);
-});
-
-svg.addEventListener('click', function(ev) {
-
-//clog(ev);
-//clog(ev.target);
-  var pt = svg.createSVGPoint(); pt.x = ev.clientX; pt.y = ev.clientY;
-  var xy =  pt.matrixTransform(svg.getScreenCTM().inverse());
-
-  clog(ev.clientX, ev.clientY);
-  clog(xy.x, xy.y);
-
-  //var vb = getViewBox();
-  //
-  //vb.x = (xy.x - vb.w / 2).toFixed(0);
-  //vb.y = (xy.y - vb.h / 2).toFixed(0);
-  //
-  //svg.setAttribute(
-  //  'viewBox',
-  //  '' + vb.x + ' ' + vb.y + ' ' + vb.w + ' ' + vb.h);
-});
-      })
+      maken(body, :script) <<
+        Ox::Raw.new(File.read(File.join(__dir__, 'svg.js')))
 
       Ox.dump(doc)
     end
